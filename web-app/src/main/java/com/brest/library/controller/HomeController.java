4906f95a8cb48b65f5d5cf3e04cd27552560a391
MaksimKushniarevich <kushnerevich.m@gmail.com>
2022-03-10 15:52:51 +0300

    add createBook and fix html

--- web-app/src/main/java/com/brest/library/controller/HomeController.java
+++ web-app/src/main/java/com/brest/library/controller/HomeController.java
@@ -1,13 +1,19 @@
 package com.brest.library.controller;
 
 
+import com.brest.library.Book;
 import com.brest.library.BookDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.sql.Struct;
+
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
     private BookDao bookDao;
@@ -17,9 +23,17 @@ public class HomeController {
         this.bookDao = bookDao;
     }
 
-    @RequestMapping("/")
-    public String getIndex(Model model){
+    @GetMapping
+    public String getBooks(Model model){
         model.addAttribute("books", bookDao.findAll());
+        model.addAttribute("newbook", new Book());
         return "books";
+    }
+
+    @PostMapping
+    public String createBook(Book book){
+        bookDao.save(book);
+        return "redirect:/";
+
     }
 }
