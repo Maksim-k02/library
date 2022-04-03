ca2305b2f19eacb1891e297f091b0caef221a08d
MaksimKushniarevich <kushnerevich.m@gmail.com>
2022-04-03 12:02:09 +0300

    fix validator for reader

--- model/src/main/java/com/brest/library/Reader.java
+++ model/src/main/java/com/brest/library/Reader.java
@@ -1,12 +1,14 @@
 package com.brest.library;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
@@ -31,6 +33,7 @@ public class Reader {
 
     private Long bookid;
 
+    @NotBlank(message = "Data should not be empty")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private String date;
